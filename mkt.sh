2ba8cd4ed940da7acaefe172172fdcddec263507
Racso <31900333+Racso20@users.noreply.github.com>
2021-10-23 21:24:06 -0300

    Update mkt.sh

--- mkt.sh
+++ mkt.sh
@@ -1,58 +1,57 @@
 function mkt(){
-
 	local OPTIND
 	local OPTARG
 	local option
-	local tipo = "HTB"
-
-	usage="$(basename "$0") [-h -m -i] [-t] -- Programa para realizar HTB ordenado
-donde:
-    -h   Muestra el menu de ayuda
-    -m   Nombre de la maquina a realizar
-    -i   IP de la maquina a realizar
-    -t   Carpeta raiz (default HTB)"
-
-    	local usuario=$(whoami)
-	while getopts 'm:i:h:' option; do
+	local tipo
+	local usage
+	local usuario
+	local maquina
+	local ip
+	local seguir
+	local cargar
+	
+	tipo="HTB"
+	usage="$((basename "$0")) [-h -m -i] [-t] -- Programa para realizar HTB ordenado
+	donde:
+	-h   Muestra el menu de ayuda
+	-m   Nombre de la maquina a realizar
+	-i   IP de la maquina a realizar
+	-t   Carpeta raiz (default HTB)"
+	local usuario=$(whoami)
+	while getopts 'm:i:h:t:' option; do
 		case "$option" in
 			h) echo "$usage"
-			exit
 			;;
-
 			m) maquina=$OPTARG
 			;;
-
 			i) ip=$OPTARG
 			;;
-
 			t) tipo=$OPTARG
 			;;
-
 			:) printf "missing argument for -%s\n" "$OPTARG" >&2
 			echo "$usage" >&2
 			;;
-
 			\?) printf "illegal option: -%s\n" "$OPTARG" >&2
 			echo "$usage" >&2
 			;;
-
-			*)printf "missing argument for -%s\n" "$OPTARG" >&2
+			*) printf "missing argument for -%s\n" "$OPTARG" >&2
 			echo "$usage" >&2
 			;;
 		esac
 	done
-
+	
 	seguir=1
 	if [ -z "$maquina" ]; then
-		echo "la maquina no puede ser nula"
+		echo "La maquina no puede ser nula"
 		seguir=0
 	fi
 	if [ -z "$ip" ]; then
 		echo "la IP no puede ser nula"
 		seguir=0
 	fi
+	
 	if [ $seguir == 1 ]; then
-		ruta="/home/$usuario/Documentos/"$tipo"/"$maquina
+		ruta="/home/$usuario/Documentos/$tipo/$maquina"
 		mkdir -p $ruta/{nmap,contenido,exploits,scripts}
 		file='/etc/hosts'
 		cargar=1
@@ -62,14 +61,16 @@ donde:
 				cargar=0
 			fi
 		done < "$file"
-
+		
 		if [ $cargar == 1 ]; then
 			echo "cargando ip en /etc/hosts"
-			comando="echo $ip	$maquina.htb"
+			comando="echo $ip       $maquina.htb"
 			$comando | tr '[:upper:]' '[:lower:]' | sudo tee -a /etc/hosts
 		fi
+		
 		cd $ruta
+		echo "Finalizo correctamente"
 	else
 		echo "$usage" >&2
 	fi
-}
+} 
